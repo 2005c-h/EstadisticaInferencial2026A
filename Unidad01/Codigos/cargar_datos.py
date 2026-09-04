3890b73c138ff0c008addcf098c51d35c4eb7f94
DULCE DIAZ <24150020@aguascalientes.tecnm.mx>
2026-09-04 12:36:07 -0600

    Add cargar_datos.py to load CSV data

---
+++ Unidad01/Codigos/cargar_datos.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+link_csv = "https://raw.githubusercontent.com/2005c-h/EstadisticaInferencial2026A/refs/heads/main/Unidad01/Problemario/Dato_problema_01.csv"
+
+df = pd.read_csv(link_csv)
+
+df
